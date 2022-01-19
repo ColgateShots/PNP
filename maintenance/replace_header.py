b9d4b193de42052918571f6fc9309d88f14a416b
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-01-19 17:05:42 +0100

    MAINT: Header replacement for Jupytext files

--- maintenance/replace_header.py
+++ maintenance/replace_header.py
@@ -24,9 +24,30 @@
 
 
 import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 
 file_lines = open(sys.argv[1], 'r').readlines()
 header_lines = sys.stdin.readlines()
+
+# keep jupytext header at top if present
+while file_lines[0].startswith('#') and not file_lines[0].startswith('# ---'):
+    file_lines = file_lines[1:]
+
+top_comment_lines = []
+# This block aims at Jupytext files and expects two '# ---' markers
+if file_lines[0].startswith('# ---'):
+    logger.debug("Found '# ---#' top comment start in file.")
+    # mimic do ... while
+    while file_lines[0].startswith('#'):
+        top_comment_lines.append(file_lines[0])
+        file_lines = file_lines[1:]
+        if file_lines[0].startswith('# ---'):
+            logger.debug("Found '# ---#' top comment end in file.")
+            logger.debug("Kept '{} top comment.".format(top_comment_lines))
+            break
+    top_comment_lines.append(file_lines[0])
 
 while file_lines[0].startswith('#'):
     file_lines = file_lines[1:]
@@ -35,5 +56,8 @@ file_lines.insert(0, '#\n')
 for header_line in header_lines[::-1]:
     file_lines.insert(0, '# {}'.format(header_line).strip() + '\n')
 file_lines.insert(0, '#\n')
+
+for top_comment_line in top_comment_lines[::-1]:
+    file_lines.insert(0, (top_comment_line).strip() + '\n')
 
 open(sys.argv[1], 'w').writelines(file_lines)
